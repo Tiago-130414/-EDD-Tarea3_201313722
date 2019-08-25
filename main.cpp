c3da378f5f902338a91c80911df418a0d2a5de00
Santiago Rivadeneira <santiriva1994@gmail.com>
2019-08-24 18:19:12 -0600

    se creo el arbol binario de busqueda

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "ArbolBB.h"
+#include <string>
+using namespace std;
+
+int main()
+{
+    ArbolBB arbolitoObj;
+    ArbolBB *arbolito = NULL;
+    arbolitoObj.insertarNodo(arbolito,"Mario1");
+    arbolitoObj.insertarNodo(arbolito,"Pikachu");
+    arbolitoObj.insertarNodo(arbolito,"Boo");
+    arbolitoObj.insertarNodo(arbolito,"Geoff");
+    arbolitoObj.insertarNodo(arbolito,"Mario2");
+    arbolitoObj.insertarNodo(arbolito,"Mushroom");
+    arbolitoObj.recorridoInO(arbolito);
+    cout<<"\n"<<"-------"<<endl;
+    arbolitoObj.recorridoPre(arbolito);
+    cout<<"\n"<<"-------"<<endl;
+    arbolitoObj.recorridoPost(arbolito);
+    return 0;
+}
